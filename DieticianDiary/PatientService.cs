ddac13ec70dcecf6fe5de929aacad94b0028d23a
Marcin Kozak <marcin.kozak.dev@gmail.com>
2023-01-10 22:15:56 +0100

    Add project files.

---
+++ DieticianDiary/PatientService.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DieticianDiary
+{
+    public class PatientService
+    {
+        public List<Patient> Patients { get; set; }
+
+        public PatientService()
+        {
+            Patients = new List<Patient>();
+        }
+
+        public int AddPatient()
+        {
+            Patient patient = new Patient();
+
+            Console.WriteLine("Please enter patient information: ");
+            Console.Write("Id: ");
+            var id = Console.ReadLine();
+            int patientId;
+            Int32.TryParse(id, out patientId);
+
+            Console.Write("First name: ");
+            var firstName = Console.ReadLine();
+
+            Console.Write("Last name: ");
+            var lastName = Console.ReadLine();
+
+            Console.Write("Age: ");
+            var age = Console.ReadLine();
+            int patientAge;
+            Int32.TryParse(id, out patientAge);
+
+            Console.Write("Email adress: ");
+            var emailAdress = Console.ReadLine();
+
+            Console.Write("Phone number: ");
+            var phoneNumber = Console.ReadLine();
+
+            patient.Id = patientId;
+            patient.FirstName = firstName;
+            patient.LastName = lastName;
+            patient.Age = patientAge;
+            patient.EmailAdress = emailAdress;
+            patient.PhoneNumber = phoneNumber;
+
+            Patients.Add(patient);
+            return patientId;
+        }
+
+        public int RemovePatientView()
+        {
+            Console.Write("Please enter id for patient you want to remove: ");
+            var patientId = Console.ReadKey();
+            int id;
+            Int32.TryParse(patientId.KeyChar.ToString(), out id);
+
+            return id;
+        }
+
+        public void RemovePatient(int removeId)
+        {
+            Patient patientToRemove = new Patient();
+            foreach (var patient in Patients)
+            {
+                if (patient.Id == removeId)
+                {
+                    patientToRemove = patient;
+                    break;
+                }
+            }
+            Patients.Remove(patientToRemove);
+        }
+    }
+}
---
+++ DieticianDiary/Program.cs
@@ -0,0 +1,61 @@
+﻿
+
+//// c.Wyświetlenie danych pacjenta
+//// d.Wyświetlenie wszystkich pacjentów
+////// a1 Zostanę poproszony o wprowadzenie wszystkich szczegółów
+////// b1 Zostane poproszony o wprowadzenie id pacjenta
+////// b2 Usunę ten produkt z listy produktów
+////// c1 Zostane poproszony o wprowadzenie id pacjenta
+////// c2 Wyświetlę wszystkie informacje związane z pacjentem
+////// d1 Wyświetlę liste pacjentów
+
+using DieticianDiary;
+
+MenuActionService actionService = new MenuActionService();
+actionService = Initialize(actionService);
+
+Console.WriteLine("Welcome to Dietician Diary app!");
+while (true)
+{
+    Console.WriteLine("\nPlease let me know what you want to do:");
+    var mainMenu = actionService.GetMenuActionByMenuName("Main");
+
+    for (int i = 0; i < mainMenu.Count; i++)
+    {
+        Console.WriteLine($"{mainMenu[i].Id}. {mainMenu[i].Name}");
+    }
+    Console.Write("\nYour choose: ");
+    var operation = Console.ReadKey();
+    Console.WriteLine("\n");
+
+    PatientService patientService = new PatientService();
+    switch (operation.KeyChar)
+    {
+        case '1':
+            var id = patientService.AddPatient();
+            break;
+        case '2':
+            var removeId = patientService.RemovePatientView();
+            patientService.RemovePatient(removeId);
+            break;
+        case '3':
+
+            break;
+        case '4':
+
+            break;
+        default:
+            Console.WriteLine("Action you entered doeas not exist");
+            break;
+    }
+}
+
+
+static MenuActionService Initialize(MenuActionService actionService)
+{
+    actionService.AddNewAction(1, "Add patient", "Main");
+    actionService.AddNewAction(2, "Remove patient", "Main");
+    actionService.AddNewAction(3, "Show patient", "Main");
+    actionService.AddNewAction(4, "List of patients", "Main");
+    return actionService;
+}
